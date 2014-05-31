f8f9588d5e32907a99172df5778f57eda43f36da
agpelliza <agpelliza@gmail.com>
2014-05-31 12:23:49 -0300

    create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fba::Application.config.secret_key_base = '740793c709d6028ab5717c745db835cfeff9dc7d21208f837b5293a8bd259f1ef4183d456d84a484d5f029551ddd1f5be90c84149b1e1f8c8f824fa06319fef1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fba::Application.config.session_store :cookie_store, key: '_fba_session'
