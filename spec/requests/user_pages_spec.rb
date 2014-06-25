d7bdbd7e8008931ffc666e1162fdd152d218a75c
agpelliza <agpelliza@gmail.com>
2014-06-25 15:15:47 -0300

    create user behaviour

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,4 +21,39 @@ describe "User pages" do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+
+  describe "create user" do
+    let(:submit) { I18n.t(:create_user) }
+    before { visit new_user_path }
+
+    describe "with invalid information" do
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title(I18n.t(:new_user_title)) }
+        it { should have_content('error') }
+      end
+
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before { fill_valid_user_information }
+
+      describe "after saving the user" do
+        let(:user) { User.find_by(email: 'user@example.com') }
+        before { click_button submit }
+
+        it { should have_title(user.name) }
+        it { should have_success_message(I18n.t(:welcome)) }
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -3,3 +3,22 @@ include ApplicationHelper
 def set_locale
   I18n.locale = I18n.default_locale
 end
+
+def fill_valid_user_information
+  fill_in I18n.t('attributes.name'), with: "Example User"
+  fill_in I18n.t('attributes.email'), with: "user@example.com"
+  fill_in I18n.t('attributes.password'), with: "foobar"
+  fill_in I18n.t('attributes.confirmation'), with: "foobar"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
