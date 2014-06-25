04a2ce45386b73fbff7db021e4da6109fbf512da
agpelliza <agpelliza@gmail.com>
2014-06-25 10:04:45 -0300

    basic user show page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "User pages" do
   before (:all) { set_locale }
   subject { page }
 
-  describe "signup page" do
+  describe "new user page" do
     let(:page_title) { I18n.t(:new_user_title) }
     let(:heading) { I18n.t(:new_user_heading) }
 
@@ -12,5 +12,13 @@ describe "User pages" do
 
     it { should have_title(full_title(page_title)) }
     it { should have_content(heading) }
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 end
