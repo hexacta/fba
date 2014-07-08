8826a2b91c3e2b6c06e9628057d57b9a6611bd70
agpelliza <agpelliza@gmail.com>
2014-07-08 10:48:01 -0300

    fix authentication hole

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   before_action :correct_user,   only: [:show, :edit, :update]
-  before_action :admin_user,     only: [:index, :destroy]
+  before_action :admin_user,     only: [:new, :create, :index, :destroy]
   
   def index
     @users = User.paginate(page: params[:page])
