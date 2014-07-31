a46e30d7ed9140fc896be8c25d6ba45ec3348abd
agpelliza <agpelliza@gmail.com>
2014-07-31 09:56:14 -0300

    enahance accessibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Fba::Application.routes.draw do
-  resources :horario_entregas
-
-  resources :donantes
-
   root  'static_pages#home'
   resources :users
   resources :roles
+  resources :horario_entregas
+  resources :donantes
   resources :sessions, only: [:new, :create, :destroy]
   match '/signin',  to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-  match '/donantes', to: 'donantes#index',     via: 'get'
-  match '/horarioEntregas', to: 'horario_entregas#index', via: 'get'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20140729002828) do
     t.boolean  "admin",           default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
